5eb5e955b2b7863e25875e01bdf66aa964e7c07e
Jennifer Asimit <jennifer@BSUs-MacBook-Pro-2.local>
2020-05-26 01:36:00 +0100

    add main files

---
+++ R/flashfm-package.R
@@ -0,0 +1,16 @@
+## usethis namespace: start
+#' @useDynLib flashfm, .registration = TRUE
+## usethis namespace: end
+#' @importFrom Rcpp sourceCpp
+#' @importFrom methods as is new
+#' @importFrom stats dbinom model.matrix sd as.dendrogram as.dist hclust is.leaf median na.omit quantile var
+#' @importFrom utils combn write.table read.table
+#' @importFrom rlist list.cbind list.stack list.append
+#' @importFrom gtools smartbind
+#' @importFrom MASS mvrnorm
+#' @importFrom snpStats ld
+#' @import MFM 
+#' @import GUESSFM
+#' @import R2BGLiMS
+#' @import Matrix
+NULL
